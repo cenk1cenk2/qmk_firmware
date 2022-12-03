26647f9d28148520fd974a67b29e11e01b641959
Cenk Kılıç <cenk@kilic.dev>
2022-12-03 18:13:21 +0100

    fix: update configuration

--- keyboards/sofle/keymaps/cenk1cenk2/config.h
+++ keyboards/sofle/keymaps/cenk1cenk2/config.h
@@ -6,6 +6,10 @@
 #define TAPPING_TERM 100
 #define ENCODER_DIRECTION_FLIP
 
+#ifdef OLED_ENABLE
+#    define OLED_TIMEOUT 30000
+#endif
+
 #define RGB_DI_PIN D3
 
 #ifdef RGBLIGHT_ENABLE
@@ -13,7 +17,7 @@
 #    define RGBLED_NUM 72 // Number of LEDs
 #    define RGBLED_SPLIT \
         { 36, 36 }
-
+#    define RGBLIGHT_DEFAULT_HUE 120
 #    define RGBLIGHT_SLEEP
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
 // #define DRIVER_LED_TOTAL 72
@@ -23,6 +27,16 @@
 #    define RGBLIGHT_HUE_STEP 10
 #    define RGBLIGHT_SAT_STEP 17
 #    define RGBLIGHT_VAL_STEP 17
+
+// disable unused modes
+#    undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    undef RGBLIGHT_EFFECT_ALTERNATING
+#    undef RGBLIGHT_EFFECT_BREATHING
+#    undef RGBLIGHT_EFFECT_CHRISTMAS
+#    undef RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    undef RGBLIGHT_EFFECT_SNAKE
 
 #    define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
 #endif
--- keyboards/sofle/keymaps/cenk1cenk2/keymap.c
+++ keyboards/sofle/keymaps/cenk1cenk2/keymap.c
@@ -8,14 +8,14 @@ enum sofle_layers {
     _ADJUST,
 };
 
-enum custom_keycodes { KC_QWERTY = SAFE_RANGE, KC_LOWER, KC_RAISE, KC_ADJUST, KC_PRVWD, KC_NXTWD, KC_LSTRT, KC_LEND, KC_DLINE };
+enum my_keycodes { KC_QWERTY = SAFE_RANGE, KC_LOWER, KC_RAISE, KC_ADJUST, KC_PRVWD, KC_NXTWD, KC_LSTRT, KC_LEND, KC_DLINE };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_QWERTY] = LAYOUT(QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, RGB_TOG, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_LGUI, KC_LALT, KC_LCTL, KC_LOWER, KC_ENT, KC_SPC, KC_RAISE, KC_RCTL, KC_RALT, KC_RGUI),
                                                               [_LOWER]  = LAYOUT(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_TRNS, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_NO, KC_F12, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_EQL, KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPLY, KC_TRNS, KC_PLUS, KC_PIPE, KC_LT, KC_GT, KC_QUES, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
                                                               [_RAISE]  = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_PGUP, KC_PRVWD, KC_UP, KC_NXTWD, KC_DLINE, KC_DEL, KC_TRNS, KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_UP, KC_MS_WH_RIGHT, KC_NO, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_ENT, KC_TRNS, KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_LSTRT, KC_NO, KC_LEND, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
                                                               [_ADJUST] = LAYOUT(QK_REBOOT, KC_NO, KC_NO, KC_NO, RGB_RMOD, RGB_MOD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_CAPS, KC_INS, KC_SCRL, CG_TOGG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_HUD, RGB_SAD, RGB_VAD, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)};
 
-#ifdef OLED_DRIVER_ENABLE
+#ifdef OLED_ENABLE
 
 static void render_logo(void) {
     static const char PROGMEM raw_logo[] = {
@@ -23,6 +23,7 @@ static void render_logo(void) {
         128, 0,   0,   0,  0,  0,   0,   24,  60,  142, 243, 255, 239, 96, 48,  56, 28, 15,  7,   0,   128, 128, 128, 128, 128, 0,  0,  0,   0,   0,   0,   0,   0,  0, 192, 240, 252, 254, 63, 15, 0, 0, 0, 0, 128, 128, 128, 128, 0, 0, 0, 0, 128, 128, 128, 128, 0, 0, 0, 128, 192, 0, 0, 0, 0, 0,  0,  0,  128, 128, 128, 128, 224, 252, 62, 15, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 48, 248, 252, 132, 129, 131, 131, 135, 207, 254, 124, 96, 252, 254, 198, 255, 121, 63, 55, 248, 252, 31, 255, 24, 24, 24, 120, 255, 223, 195, 97, 120, 252, 255, 223, 205, 207, 103, 51, 24, 0, 0, 0, 0, 128, 224, 252, 63,  7,   15,  28, 120, 240, 224, 192, 240, 252, 255, 223, 205, 207, 103, 51, 124, 127, 103, 240, 248, 254, 222, 120, 255, 255, 131, 223, 246, 126, 60,  112, 252, 254,
         198, 255, 121, 63, 55, 248, 252, 222, 199, 99,  249, 253, 223, 99, 112, 28, 15, 251, 255, 222, 192, 240, 252, 254, 199, 99, 49, 253, 255, 199, 192, 112, 24, 0, 0,   0,   0,   0,   0,  0,  0, 0, 0, 0, 0,   0,   0,   0,   0, 1, 1, 1, 1,   1,   1,   1,   0, 0, 0, 0,   0,   0, 0, 0, 0, 31, 31, 27, 15,  7,   0,   0,   0,   0,   0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  1,   1,   0,   0,   0,   0,   0,   0,   0,   1,   1,  1,   0,   0,   0,   0,   0,  0,  28,  30,  26, 31,  15, 3,  0,  0,   0,   1,   1,   1,  1,   0,   0,   0,   0,   0,   0,   0,  0,  0, 0, 0, 0, 0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
     };
+
     oled_write_raw_P(raw_logo, sizeof(raw_logo));
 }
 
@@ -56,6 +57,7 @@ static void print_status_narrow(void) {
         default:
             oled_write_ln_P(PSTR("Undef"), false);
     }
+
     oled_write_P(PSTR("\n\n"), false);
     led_t led_usb_state = host_keyboard_led_state();
     oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
@@ -64,17 +66,29 @@ static void print_status_narrow(void) {
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     if (is_keyboard_master()) {
         return OLED_ROTATION_270;
-    } else {
-        return OLED_ROTATION_180;
     }
+
+    return OLED_ROTATION_180;
 }
 
-void oled_task_user(void) {
+bool oled_task_user(void) {
     if (is_keyboard_master()) {
         print_status_narrow();
     } else {
         render_logo();
     }
+
+    return false;
+}
+
+void suspend_power_down_user(void) {
+    oled_off();
+    rgblight_disable();
+}
+
+void suspend_wakeup_init_user(void) {
+    oled_on();
+    rgblight_enable();
 }
 
 #endif
