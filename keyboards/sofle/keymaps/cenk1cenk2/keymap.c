f01593a8ec6d3b36becfd571f4cb5ab5fffc32c5
Cenk Kılıç <cenk@kilic.dev>
2023-05-31 20:02:05 +0200

    fix: update keymap

--- keyboards/sofle/keymaps/cenk1cenk2/keymap.c
+++ keyboards/sofle/keymaps/cenk1cenk2/keymap.c
@@ -31,9 +31,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         case _LOWER:
             if (index == 0) {
                 if (clockwise) {
-                    tap_code(KC_MPRV);
-                } else {
                     tap_code(KC_MNXT);
+                } else {
+                    tap_code(KC_MPRV);
                 }
             } else if (index == 1) {
                 if (clockwise) {
