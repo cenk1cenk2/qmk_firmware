1ff94d80e63bafe7ea1402d2d516ad53e779fafd
Cenk Kılıç <cenk@kilic.dev>
2024-01-26 00:39:29 +0100

    chore: format better

--- keyboards/sofle/keymaps/cenk1cenk2/keymap.c
+++ keyboards/sofle/keymaps/cenk1cenk2/keymap.c
@@ -9,28 +9,28 @@ QK_GESC,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,               
 KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                               KC_Y,        KC_U,      KC_I,      KC_O,       KC_P,        KC_MINS,
 KC_LCTL,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                               KC_H,        KC_J,      KC_K,      KC_L,       KC_SCLN,     KC_QUOT,
 KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_MUTE,    RGB_TOG,    KC_N,        KC_M,      KC_COMM,   KC_DOT,     KC_SLSH,     KC_RSFT,
-              KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LOWER,   KC_ENT,                   KC_SPC,     KC_RAISE,    KC_RCTL,    KC_RALT,   KC_RGUI
+              KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LOWER,   KC_ENT,                     KC_SPC,     KC_RAISE,    KC_RCTL,    KC_RALT,   KC_RGUI
 ),
 [_LOWER]  = LAYOUT(
 KC_GRV,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                              KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,
 KC_TRNS,    KC_LCBR,    KC_RCBR,    KC_LBRC,    KC_RBRC,    KC_NO,                              KC_F12,     KC_LCBR,    KC_RCBR,    KC_LBRC,    KC_RBRC,    KC_EQL,
 KC_TRNS,    KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,                            KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_BSLS,
 KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_MPLY,    KC_TRNS,    KC_PLUS,    KC_PIPE,    KC_LT,      KC_GT,      KC_QUES,    KC_TRNS,
-              KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
+              KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
 ),
 [_RAISE]  = LAYOUT(
 KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                            KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
 KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_PSCR,                            KC_PGUP,    KC_PRVWD,   KC_UP,      KC_NXTWD,   KC_DLINE,   KC_DEL,
 KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                              KC_PGDN,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_NO,      KC_ENT,
 KC_TRNS,    KC_UNDO,    KC_CUT,     KC_COPY,    KC_PASTE,   KC_NO,      KC_TRNS,    KC_TRNS,    KC_NO,      KC_HOME,    KC_NO,      KC_END,     KC_NO,      KC_TRNS,
-              KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
+              KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
 ),
 [_ADJUST] = LAYOUT(
 QK_REBOOT,  KC_NO,      KC_NO,      KC_NO,      RGB_RMOD,   RGB_MOD,                            KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
 KC_NO,      KC_CAPS,    KC_INS,     KC_SCRL,    CG_TOGG,    KC_NO,                              KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
 KC_NO,      RGB_HUI,    RGB_SAI,    RGB_VAI,    KC_NO,      KC_NO,                              KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
 KC_NO,      RGB_HUD,    RGB_SAD,    RGB_VAD,    KC_NO,      KC_NO,      KC_TRNS,    KC_TRNS,    KC_NO,      KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_NO,      KC_NO,
-              KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
+              KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
 )
 };
 
