af8426268babd0070183b13dc03e92f7a53ae7e3
Cenk Kılıç <cenk@kilic.dev>
2024-01-29 14:39:04 +0100

    fix: update keymap

--- keyboards/sofle/keymaps/cenk1cenk2/keymap.c
+++ keyboards/sofle/keymaps/cenk1cenk2/keymap.c
@@ -6,8 +6,8 @@
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
-QK_GESC,            KC_1,                 KC_2,                 KC_3,               KC_4,                KC_5,                                                          KC_6,                KC_7,               KC_8,                 KC_9,               KC_0,               KC_MINS,
-KC_TAB,             KC_Q,                 KC_W,                 KC_E,               KC_R,                KC_T,                                                          KC_Y,                KC_U,               KC_I,                 KC_O,               KC_P,               KC_BSPC,
+QK_GESC,            KC_1,                 KC_2,                 KC_3,               KC_4,                KC_5,                                                          KC_6,                KC_7,               KC_8,                 KC_9,               KC_0,               KC_BSPC,
+KC_TAB,             KC_Q,                 KC_W,                 KC_E,               KC_R,                KC_T,                                                          KC_Y,                KC_U,               KC_I,                 KC_O,               KC_P,               KC_MINS,
 LCTL_T(KC_TAB),     KC_A,                 KC_S,                 KC_D,               KC_F,                KC_G,                                                          KC_H,                KC_J,               KC_K,                 KC_L,               KC_SCLN,            KC_QUOT,
 KC_LSFT,            KC_Z,                 KC_X,                 KC_C,               KC_V,                KC_B,                KC_MUTE,             RGB_TOG,             KC_N,                KC_M,               KC_COMM,              KC_DOT,             KC_SLSH,            KC_RSFT,
                     KC_LGUI,              KC_LALT,              KC_LCTL,            MO(_LOWER),          KC_ENT,                                                        KC_SPC,              MO(_RAISE),         KC_RCTL,              KC_RALT,            KC_RGUI
@@ -20,10 +20,10 @@ KC_TRNS,            KC_BSLS,              KC_LBRC,              KC_RBRC,        
                     KC_TRNS,              KC_TRNS,              KC_TRNS,            KC_TRNS,             KC_TRNS,                                                       KC_TRNS,             KC_TRNS,            KC_TRNS,              KC_TRNS,            KC_TRNS
 ),
 [_RAISE]  = LAYOUT(
-KC_TRNS,            KC_NO,                KC_NO,                KC_NO,              KC_NO,               KC_NO,                                                         KC_NO,               KC_NO,              KC_NO,                KC_NO,              KC_NO,               KC_NO,
-KC_TRNS,            KC_F1,                KC_F2,                KC_F3,              KC_F4,               KC_F5,                                                         KC_F6,               LCTL(KC_LEFT),      KC_UP,                LCTL(KC_RIGHT),     KC_PGUP,             KC_DEL,
-KC_TRNS,            KC_F7,                KC_F8,                KC_F9,              KC_F10,              KC_F11,                                                        KC_F12,              KC_LEFT,            KC_DOWN,              KC_RGHT,            KC_PGDN,             KC_ENT,
-KC_TRNS,            KC_NO,                KC_NO,                KC_NO,              KC_NO,               KC_NO,               KC_TRNS,             KC_TRNS,             KC_PSCR,             KC_HOME,            KC_NO,                KC_END,             KC_NO,               KC_NO,
+KC_TRNS,            KC_NO,                KC_NO,                KC_NO,              KC_NO,               KC_NO,                                                         KC_NO,               KC_NO,              KC_NO,                KC_NO,              KC_NO,               KC_TRNS,
+KC_TRNS,            KC_F1,                KC_F2,                KC_F3,              KC_F4,               KC_NO,                                                         KC_NO,               LCTL(KC_LEFT),      KC_UP,                LCTL(KC_RIGHT),     KC_PGUP,             KC_DEL,
+KC_TRNS,            KC_F5,                KC_F6,                KC_F7,              KC_F8,               KC_NO,                                                         KC_NO,               KC_LEFT,            KC_DOWN,              KC_RGHT,            KC_PGDN,             KC_ENT,
+KC_TRNS,            KC_F9,                KC_F10,               KC_F11,             KC_F12,              KC_NO,               KC_TRNS,             KC_TRNS,             KC_PSCR,             KC_HOME,            KC_NO,                KC_END,             KC_NO,               KC_NO,
                     KC_TRNS,              KC_TRNS,              KC_TRNS,            KC_TRNS,             KC_TRNS,                                                       KC_TRNS,             KC_TRNS,            KC_TRNS,              KC_TRNS,            KC_TRNS
 ),
 [_ADJUST] = LAYOUT(
