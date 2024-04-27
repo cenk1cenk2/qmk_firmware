926890213b66dd30f821cf839a79a4ee39b63a8b
Cenk Kılıç <cenk@kilic.dev>
2024-04-27 16:51:02 +0200

    fix: update configuration

--- keyboards/sofle/keymaps/cenk1cenk2/config.h
+++ keyboards/sofle/keymaps/cenk1cenk2/config.h
@@ -38,7 +38,7 @@
 
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLIGHT_SPLIT
-#    define RGBLED_NUM 72 // Number of LEDs
+#    define RGBLIGHT_LED_COUNT 72 // Number of LEDs
 #    define RGBLED_SPLIT \
         { 36, 36 }
 #    define RGBLIGHT_DEFAULT_HUE 120
